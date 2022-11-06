e4c45e59f60f5a8a7cf7a9d3d9d51978a17b9f95
Crisley Alves <crisleyalvesphx@gmail.com>
2022-11-06 11:35:03 -0300

    update customFetch necessary params

--- social-network state management/src/utils/customFetch.js
+++ social-network state management/src/utils/customFetch.js
@@ -1,9 +1,6 @@
 import { getToken } from "./localStorageUtils"
 
-export default function ({
-  endpoint,
-  body = {}
-}) {
+export default function (endpoint, body = {}) {
   const token = getToken()
   return fetch(endpoint, { headers: `Bearer ${token}`, body })
 }
